ff36cb4dd72d22e445a7963755c92cd2abd102c1
AAD2001 <53010298+AAD2001@users.noreply.github.com>
2019-07-24 10:47:38 -0500

    j

--- app/src/main/java/com/example/core_camp/Acttwo.java
+++ app/src/main/java/com/example/core_camp/Acttwo.java
@@ -1,14 +1,37 @@
 package com.example.core_camp;
 
+import androidx.appcompat.R;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Acttwo extends AppCompatActivity {
+
+    public Button butl;
+
+    public void init (){
+        butl= (Button)findViewById(R.id.butl);
+        butl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent toy = new Intent(first.this,second.class);
+
+
+            }
+        });
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acttwo);
+        init();
     }
 }
