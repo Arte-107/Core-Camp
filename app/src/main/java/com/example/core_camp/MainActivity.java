0ba57079b1925cde0e59a6021ec01a473a877e22
stephaniecastro <53010504+stephaniecastro@users.noreply.github.com>
2019-07-31 10:33:40 -0500

    long

--- app/src/main/java/com/example/core_camp/MainActivity.java
+++ app/src/main/java/com/example/core_camp/MainActivity.java
@@ -10,51 +10,29 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button BtnMove;
+    private Button languageActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
 
+         languageActivity = findViewById(R.id.BtnActivityOne);
+         languageActivity.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(view.getContext(), languageActivity.class);
+                 startActivity(intent);
+             }
+         });
 
-            }
-        private Button languageActivity;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            Button button=findViewById(R.id.BtnActivityOne);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view){
-                    Intent intent = new Intent(view.getContext(),languageActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
-        
-        
-        
-        private Button accountsActivity;
-        
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setConetentView(R.layout.activity_main);
-            
-            Button button=findViewById(R.id.accountb123);
-            button.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
-                    Intent intent = new Intent(view.getContext(),accountsActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
-
-    private void setConetentView(int activity_main) {
-    }
-
+         BtnMove = findViewById(R.id.accountb123);
+         BtnMove.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(view.getContext(), accountsActivity.class);
+                 startActivity(intent);
+             }
+         });
+     }
 }
