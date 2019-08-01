9773dbb86201c6459d5ff890cfb35bddb4605384
FranciscoO0920 <53010347+FranciscoO0920@users.noreply.github.com>
2019-08-01 11:01:11 -0500

    wow

--- app/src/main/java/com/example/core_camp/Activity2.java
+++ app/src/main/java/com/example/core_camp/Activity2.java
@@ -2,13 +2,46 @@ package com.example.core_camp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
-
+private Button button1;
+private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                openKrabs();
+            }
+        });
+    }
+        public void openKrabs(){
+            Intent intent = new Intent(this,Krabs.class);
+            startActivity(intent);
+
+        }
+    protected void onCreate2(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                opencookies();
+            }
+        });
+    }
+    public void opencookies(){
+        Intent intent = new Intent(this,Krabs.class);
+        startActivity(intent);
+
     }
 }
+
