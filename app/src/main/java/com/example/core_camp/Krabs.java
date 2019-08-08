89e03e558b55dd759edf9a5ed3169f5d1fe57d7e
FranciscoO0920 <53010347+FranciscoO0920@users.noreply.github.com>
2019-08-08 09:04:33 -0500

    frhgv

--- app/src/main/java/com/example/core_camp/Krabs.java
+++ app/src/main/java/com/example/core_camp/Krabs.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Krabs extends AppCompatActivity {
-private Button button30;
+    private Button button30;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
