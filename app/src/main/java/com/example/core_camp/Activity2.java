0f1a569c0822da5dd38b3535757bafce41f5eca3
stephaniecastro <53010504+stephaniecastro@users.noreply.github.com>
2019-08-08 12:18:09 -0500

    clubs

--- app/src/main/java/com/example/core_camp/Activity2.java
+++ app/src/main/java/com/example/core_camp/Activity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
@@ -12,28 +13,8 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        Button cars = findViewById(R.id.k);
-        cars.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
 
-            Intent intent = new Intent(view.getContext(), k.class);
-
-            startActivity(intent);
-
-        });
-
-        Button kars = findViewById(R.id.r);
-        kars.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-
-            Intent intent = new Intent(view.getContext(), r.class);
-
-            startActivity(intent);
-
-        });
+        WebView pop = (WebView) findViewById(R.id.clu);
+        pop.loadUrl("https://www.utrgv.edu/cstem/resources/student/student-organizations-at-utrgv/index.htm");
     }
 }
